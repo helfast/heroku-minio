8aae42bb7507a85bfba8325d73b2f62a0d444b06
Krishna Chaitanya <capriciousduck@gmail.com>
2020-08-21 14:13:56 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@ wget --no-check-certificate https://dl.minio.io/server/minio/release/linux-amd64
 chmod +x minio
 export MINIO_ACCESS_KEY="$ACCESS_KEY"
 export MINIO_SECRET_KEY="$SECRET_KEY"
+export MINIO_BROWSER="$MINIO_BROWSER_STATUS"
 ./minio server --address :$PORT /mnt/minio/data
